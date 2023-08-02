de93552a72c7f6a663576b7ee9798acb12b9b0b5
nkobayasi <noriyuki.kobayasi@gmail.com>
2023-08-02 18:43:18 +0900

    解析とタイプの追加修正

--- ping_analytics.py
+++ ping_analytics.py
@@ -5,6 +5,42 @@ import time
 import sqlite3
 import ping as pinglib
 
+class PingResult(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def factory(cls, ip: IpPacket):
+        echo_reply = EchoReply.factory(ip.payload)
+        self = cls()
+        self.addr = ip.src_addr
+        self.roundtrip = (time.time() - echo_reply.epoch) * 1000.0
+        self.size = ip.payload_size
+        self.ttl = ip.ttl
+        return self
+
+class Pings(object):
+    pass
+
+class PingsStatic(object):
+    def __init__(self, results):
+        self.results = results
+
+    def __str__(self):
+        return '{!s}, min: {:.2f}, max: {:.2f}, avg: {:.2f}'.format(self.results, self.min, self.max, self.avg)
+        
+    @property
+    def min(self):
+        return min(map(lambda _: _['roundtrip'], self.results))
+
+    @property
+    def max(self):
+        return max(map(lambda _: _['roundtrip'], self.results))
+
+    @property
+    def avg(self):
+        return sum(map(lambda _: _['roundtrip'], self.results)) / len(self.results)
+
 class PingAnalytics(object):
     def __init__(self):
         self.db = sqlite3.connect('ping_analytics.db')
@@ -27,6 +63,14 @@ class PingAnalytics(object):
             result['size'],
             result['roundtrip'],
             result['ttl'],
+            int(time.time()), ))
+        self.db.commit();
+
+    def failure(self, result):
+        cursor = self.db.cursor()
+        cursor.execute("""INSERT INTO histories(error, addr, error_string, epoch) VALUES(true, ?, ?, ?)""", (
+            result['addr'],
+            result['error'],
             int(time.time()), ))
         self.db.commit();
         
@@ -53,12 +97,17 @@ class PingAnalytics(object):
 def main():
     ping = pinglib.Ping()
     analytics = PingAnalytics()
-    for _ in range(60 * 10):
-        analytics.record(ping.execute('127.0.0.1'))
-        analytics.record(ping.execute('192.168.12.1'))
-        analytics.record(ping.execute('1.1.1.1'))
-        analytics.record(ping.execute('8.8.8.8'))
-        analytics.record(ping.execute('google.com'))
+    for _ in range(60 * 60 * 6):
+        try:
+            analytics.record(ping.execute('127.0.0.1'))
+            analytics.record(ping.execute('192.168.12.1'))
+            analytics.record(ping.execute('1.1.1.1'))
+            analytics.record(ping.execute('8.8.8.8'))
+            analytics.record(ping.execute('google.com'))
+        except pinglib.PingTimeout as e:
+            analytics.failure({'addr': e.addr, 'error': e.message})
+        except pinglib.PingError as e:
+            analytics.failure({'addr': e.ip.src_addr.compressed, 'error': e.message})
         time.sleep(1.0)
     print(analytics.result)
     print(analytics.jitter)
