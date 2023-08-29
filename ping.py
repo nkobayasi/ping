bac3ef150d81b361e5f948e8bb170a24a62f787d
nkobayasi <noriyuki.kobayasi@gmail.com>
2023-08-30 01:00:20 +0900

    スペルミス修正

--- ping.py
+++ ping.py
@@ -297,13 +297,13 @@ class Ping(object):
                 raise DestinationUnreachable(ip=ip)
             if echo_reply.header['id']:
                 if echo_reply.header['type'] == IcmpType.ECHO_REQUEST:
-                    logger.debug('"ECHO_REQUEST" received. Packet filtered out.')
+                    logger.debug('Received "ECHO_REQUEST". Packet filtered.')
                     continue
                 if echo_reply.id != echo_request.id:
-                    logger.debug('Mismatch ICMP ID. Packet filtered out.')
+                    logger.debug('Mismatch ICMP ID. Packet filtered.')
                     continue
                 if echo_reply.seq != echo_request.seq:
-                    logger.debug('Mismatch IMCP Sequence. Packet filtered out.')
+                    logger.debug('Mismatch ICMP Sequence. Packet filtered.')
                     continue
             if echo_reply.header['type'] == IcmpType.ECHO_REPLY:
                 return {
