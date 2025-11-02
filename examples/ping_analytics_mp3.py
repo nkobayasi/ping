b7ea9591278bb85d6e9111e5b2b7356ef1f791df
nkobayasi <noriyuki.kobayasi@gmail.com>
2025-11-02 12:07:40 +0900

    マルチプロセスによる処理サンプル

---
+++ examples/ping_analytics_mp3.py
@@ -0,0 +1,131 @@
+#!/usr/local/bin/python3
+# encoding: utf-8
+
+import sys
+import signal
+import time
+import statistics
+import sqlite3
+import logging
+import logging.handlers
+import threading
+import multiprocessing
+import traceback
+import ping as pinglib
+
+class StderrHandler(logging.StreamHandler):
+    def __init__(self):
+        super().__init__()
+        self.setFormatter(logging.Formatter('[%(process)d] %(message)s'))
+
+class StdoutHandler(logging.StreamHandler):
+    def __init__(self):
+        super().__init__(stream=sys.stdout)
+        self.setFormatter(logging.Formatter('%(message)s'))
+
+class SyslogHandler(logging.handlers.SysLogHandler):
+    def __init__(self):
+        super().__init__()
+        self.setFormatter(logging.Formatter('%(levelname)s: %(name)s.%(funcName)s(): %(message)s'))
+
+class FileHandler(logging.handlers.WatchedFileHandler):
+    def __init__(self, filename):
+        super().__init__(filename, encoding='utf-8')
+        self.setFormatter(logging.Formatter('[%(asctime)s] [%(process)d] %(levelname)s: %(name)s.%(funcName)s(): %(message)s'))
+
+logger = logging.getLogger('ping').getChild('analytics')
+logger.addHandler(StderrHandler())
+logger.setLevel(logging.DEBUG)
+
+class PingAnalytics(object):
+    def __init__(self):
+        self.db = sqlite3.connect('ping_analytics.db')
+        cursor = self.db.cursor()
+        cursor.execute("PRAGMA journal_mode = WAL")
+        cursor.execute("PRAGMA synchronous = normal")
+        cursor.execute("PRAGMA journal_size_limit = 6144000")
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS histories(
+                error boolean,
+                error_string text,
+                addr varchar(15),
+                size integer,
+                roundtrip float,
+                ttl integer,
+                epoch datetime)""")
+        cursor.execute("CREATE INDEX IF NOT EXISTS histories_error_idx ON histories(error)")
+        cursor.execute("CREATE INDEX IF NOT EXISTS histories_addr_idx ON histories(addr)")
+        cursor.execute("CREATE INDEX IF NOT EXISTS histories_epoch_idx ON histories(epoch)")
+        cursor.execute("CREATE INDEX IF NOT EXISTS histories_epoch_addr_idx ON histories(epoch, addr)")
+        self.db.commit()
+
+    def record(self, results):
+        epoch, result = results
+        if 'error' in result:
+            self.failure(epoch, result)
+        else: 
+            self.success(epoch, result)
+    
+    def success(self, epoch, result):
+        print('{addr} からの応答: バイト数 ={size} 時間 ={rtt:.1f}ms TTL={ttl}'.format(
+            addr=result['addr'].compressed,
+            size=result['size'],
+            rtt=result['roundtrip'],
+            ttl=result['ttl']))
+        cursor = self.db.cursor()
+        cursor.execute("""INSERT INTO histories(error, addr, size, roundtrip, ttl, epoch) VALUES(false, ?, ?, ?, ?, ?)""", (
+            result['addr'].compressed,
+            result['size'],
+            result['roundtrip'],
+            result['ttl'],
+            int(epoch), ))
+        self.db.commit();
+
+    def failure(self, epoch, result):
+        print('{addr} からの応答: {err}'.format(addr=result['addr'], err=result['error']))
+        cursor = self.db.cursor()
+        cursor.execute("""INSERT INTO histories(error, addr, error_string, epoch) VALUES(true, ?, ?, ?)""", (
+            result['addr'],
+            result['error'],
+            int(epoch), ))
+        self.db.commit();
+        
+class PingMT(multiprocessing.Process):
+    def __init__(self, resultq, target, terminated):
+        super().__init__()
+        self.target = target
+        self.resultq = resultq
+        self.terminated = terminated
+        
+    def run(self):
+        ping = pinglib.Ping()
+        while not self.terminated.wait(timeout=1.0):
+            epoch = time.time()
+            try:
+                self.resultq.put((epoch, ping.execute(self.target)))
+            except (pinglib.PingTimeout, pinglib.HostUnknown) as e:
+                self.resultq.put((epoch, {'addr': e.addr, 'error': e.message}))
+            except pinglib.PingError as e:
+                self.resultq.put((epoch, {'addr': e.ip.src_addr.compressed, 'error': e.message}))
+            except Exception as e:
+                logger.error(traceback.format_exception(e))
+            
+def main():
+    terminated = multiprocessing.Event()
+    resultq = multiprocessing.Queue()
+    for target in ['127.0.0.1', '192.168.0.1', '1.1.1.1', '1.0.0.1', '8.8.8.8', '8.8.4.4', 'www.google.com', 'www.youtube.com']:
+    #for target in ['127.0.0.1', '192.168.0.1', '8.8.8.8']:
+        PingMT(resultq=resultq, target=target, terminated=terminated).start()
+    #
+    analytics = PingAnalytics()
+    try:
+        while True:
+            analytics.record(resultq.get())
+    except KeyboardInterrupt:
+        terminated.set()
+    except Exception as e:
+        terminated.set()
+        logger.error(traceback.format_exception(e))
+
+if __name__ == '__main__':
+    main()
